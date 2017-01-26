0d731bd54c4ac6e3559d1badc54a5d036fa96d4f
Gumeez <at.alex.tomas@gmail.com>
2017-01-25 19:34:31 -0500

    Add files via upload

---
+++ semester 1/sheet 1/javascript.js
@@ -0,0 +1,20 @@
+function semOne() {
+    "use strict";
+    window.alert('enjoy reading about my teachers');
+    document.getElementById("semester").innerHTML = "Semester One";
+    document.getElementById("btn1").style.display = "none";
+    document.getElementById("btn2").style.display = "none";
+    document.getElementById("listOne").style.display = "block";
+    document.getElementById("listTwo").style.display = "none";
+    document.getElementById("home").style.display = "block";
+}
+function semTwo() {
+    "use strict";
+    window.alert('enjoy reading about more teachers');
+    document.getElementById("semester").innerHTML = "Semester Two";
+    document.getElementById("btn1").style.display = "none";
+    document.getElementById("btn2").style.display = "none";
+    document.getElementById("listTwo").style.display = "block";
+    document.getElementById("listOne").style.display = "none";
+    document.getElementById("home").style.display = "block";
+}
